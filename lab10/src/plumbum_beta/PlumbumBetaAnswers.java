c1dd91aeec8fb601fbdbfe32514923ed2937f7c2
Jacqueline Chu <jackiechu@berkeley.edu>
2018-10-25 11:00:55 -0700

    lab 10 finished

--- lab10/src/plumbum_beta/PlumbumBetaAnswers.java
+++ lab10/src/plumbum_beta/PlumbumBetaAnswers.java
@@ -1,13 +1,16 @@
 package plumbum_beta;
 
 public class PlumbumBetaAnswers {
-	public static final String answerToQuestion1A = "FILL THIS IN";
-	public static final String answerToQuestion1B = "FILL THIS IN";
-	public static final String answerToQuestion1C = "FILL THIS IN";
+	public static final String answerToQuestion1A = "Null Pointer Exception";
+	public static final String answerToQuestion1B = "Line 24";
+	public static final String answerToQuestion1C = "Top";
 
-	public static final String answerToQuestion2 = "FILL THIS IN";
+	public static final String answerToQuestion2 = "The Array List called entries was not initialized which caused" +
+			"the error. Thus, to resolve the issue, you must initialize the a new Array List that consists of" +
+			"Entry called entries";
 
-	public static final String answerToQuestion3A = "FILL THIS IN";
-	public static final String answerToQuestion3B = "FILL THIS IN";
-	public static final String answerToQuestion3C = "FILL THIS IN";
+	public static final String answerToQuestion3A = "Line 59";
+	public static final String answerToQuestion3B = "The bug is an index out of bounds exception where the index 5" +
+			"is out of bounds for length 5 and you must compare entry sizes instead of just size.";
+	public static final String answerToQuestion3C = "Line 58";
 }
--- lab10/src/plumbum_beta/SparseIntVector.java
+++ lab10/src/plumbum_beta/SparseIntVector.java
@@ -18,7 +18,7 @@ public class SparseIntVector {
 
 	public SparseIntVector(int... values) {
 		// replace this comment with something?
-
+		entries = new ArrayList<Entry>();
 		for (int i = 0; i < values.length; i++) {
 			if (values[i] != 0) {
 				entries.add(new Entry(i, values[i]));
@@ -55,7 +55,7 @@ public class SparseIntVector {
 		int value = 0;
 		int indexA = 0;
 		int indexB = 0;
-		while (indexA <= a.size() && indexB <= b.size()) {
+		while (indexA < a.entries.size() && indexB < b.entries.size()) {
 			Entry entryA = a.entries.get(indexA);
 			Entry entryB = b.entries.get(indexB);
 
