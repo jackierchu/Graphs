41223e59fb165eb37d27977ffed7efa6699fc6b0
Jacqueline Chu <jackiechu@berkeley.edu>
2018-12-03 18:52:40 -0800

    new

--- proj3/graph/GraphObj.java
+++ proj3/graph/GraphObj.java
@@ -28,25 +28,21 @@ abstract class GraphObj extends Graph {
         _vertices = new ArrayList<Integer>();
         _edges = new ArrayList<int[]>();
         _adjList = new ArrayList<ArrayList<Integer>>();
-        // FIXME FIXED
     }
 
     @Override
     public int vertexSize() {
         return _vertices.size();
-        // FIXME FIXED
     }
 
     @Override
     public int maxVertex() {
         return max(_vertices);
-        // FIXME FIXED
     }
 
     @Override
     public int edgeSize() {
         return _edges.size();
-        // FIXME FIXED
     }
 
     @Override
@@ -54,9 +50,6 @@ abstract class GraphObj extends Graph {
 
     @Override
     public int outDegree(int v) {
-//        int idx = _vertices.indexOf(v);
-//        int result = _adjList.get(idx).size();
-//        return result;
         ArrayList<Integer> filled = new ArrayList<>();
         int maxValue = max(_vertices);
         for (int i=1; i<=maxValue; i++){
@@ -71,7 +64,6 @@ abstract class GraphObj extends Graph {
             return 0;
         }
         return _adjList.get(vIndex).size();
-        // FIXME FIXED
     }
 
     @Override
@@ -80,20 +72,10 @@ abstract class GraphObj extends Graph {
     @Override
     public boolean contains(int u) {
         return _vertices.contains(u);
-        // FIXME FIXED
     }
 
     @Override
     public boolean contains(int u, int v) {
-//        if(!contains(u) || !contains(v)){
-//            return false;
-//        }
-//        int uIdx = _vertices.indexOf(u);
-//        int vIdx = _vertices.indexOf(v);
-//
-//        ArrayList<Integer> uValue = _adjList.get(uIdx);
-//        ArrayList<Integer> vValue = _adjList.get(vIdx);
-//        return (uValue.contains(u) || vValue.contains(v));
         if (!contains(u) || !contains(v)) return false;
         for (int[] e : _edges) {
             if ((e[0] == u  && e[1] == v)
@@ -102,7 +84,6 @@ abstract class GraphObj extends Graph {
             }
         }
         return false;
-        // FIXME FIXED
     }
 
     /** Implemented method. */
@@ -110,129 +91,45 @@ abstract class GraphObj extends Graph {
 
     @Override
     public int add() {
-//        ArrayList<Integer> list = new ArrayList<>(_vertices);
-//        int min = findMin(list);
-//        resize(min);
-//        _vertices.add(min);
-//        _adjList.add(min, new ArrayList<>());
-//        return min;
         int curMin;
-        // empty or first add
         if (_vertices.size() == 0) {
             curMin = 1;
         } else {
-            // get the current min in _vertices
             curMin = Collections.min(_vertices);
-            // a look back to check from 1 to curMin has a slot
             curMin = checkEmptySlot(curMin);
-            // find next available integer not in current _vertices list
             while (_vertices.contains(curMin)) {
                 curMin++;
             }
         }
-        //if curMin <= 1 just return the current and do nothing
         _vertices.add(curMin);
-        //add an empty list to adjList
         _adjList.add(new ArrayList<>());
         return curMin;
-        // FIXME FIXED
     }
 
     /** NEW Implemented helper method. */
     private int checkEmptySlot(int max) {
         int minSlot = max;
         for (int i = 1; i <=max ; i++) {
-            if (!_vertices.contains(i)){
+            if (!_vertices.contains(i)) {
                 return i;
             }
         }
         return minSlot+1;
     }
 
-
-    /** Implemented Helper Function. */
-    private void resize(int x) {
-        int current = vertexSize();
-        if (x >= current) {
-            while (_adjList.size() <= x) {
-                _adjList.add(new ArrayList<>());
-            }
-        }
-    }
-    /** Implemented Helper Function. */
-    private int findMin(ArrayList<Integer> vertices){
-        int size = vertices.size();
-        for(int i = 0; i < size; i++){
-            int value = Math.abs(vertices.get(i));
-            if(value - 1 < size && vertices.get(value - 1) > 0){
-                int reverse = - vertices.get(value - 1);
-                vertices.set(value - 1, reverse);
-            }
-        }
-        for(int i = 0; i < size; i++){
-            if (vertices.get(i) > 0) {
-                return i+1;
-            }
-        }
-        return size + 1;
-    }
-
     @Override
     public int add(int u, int v) {
-//        if(!contains(u)) {
-//            _vertices.add(u);
-//            ArrayList<Integer> incomingU = new ArrayList<Integer>();
-//            resize(u);
-//            incomingU.add(v);
-//            _adjList.add(u, incomingU);
-//        } else if (!contains(u, v)) {
-//            ArrayList<Integer> incomingU = _adjList.get(u);
-//            incomingU.add(v);
-//        }
-//
-//        if (!contains(v)) {
-//            _vertices.add(v);
-//        }
-//
-//        if (!edgeContains(u, v)) {
-//            _edges.add(new int[]{u, v});
-//        }
-//
-//        if (!isDirected()) {
-//            resize(v);
-//            ArrayList<Integer> incomingV = _adjList.get(v);
-//
-//            if (incomingV != null) {
-//                if (!incomingV.contains(u)) {
-//                    incomingV.add(u);
-//                }
-//            }
-//            else {
-//                ArrayList<Integer> tempV = new ArrayList<>();
-//                tempV.add(u);
-//                _adjList.add(v, tempV);
-//            }
-//        }
-//
-//        return edgeId(u, v);
-        // Leaves U enter V in directed graph
         if (isDirected()) {
-            // Add an edge incident on U and V
             if(!edgeContains(u, v)){
                 _edges.add(new int[]{u, v});
             }
-            // Get index of u
             int uIndex = _vertices.indexOf(u);
-            // Get u's adj list
             ArrayList<Integer> uAdjs = _adjList.get(uIndex);
             uAdjs.add(v);
-            // update _adjList
             _adjList.set(uIndex, uAdjs);
-            // update _edges
-        } else { //undirected graph is two way
+        } else {
             int min = Math.min(u,v);
             int max = Math.max(u,v);
-            // don't want to add two incidents in _edges
             if(!edgeContains(min, max)){
                 _edges.add(new int[]{min, max});
             }
@@ -250,76 +147,29 @@ abstract class GraphObj extends Graph {
             _adjList.set(uIndex, uAdjs);
             _adjList.set(vIndex, vAdjs);
         }
-        // Returns a unique positive number identifying the edge
         return edgeId(u, v);
     }
 
     @Override
     public void remove(int v) {
-//        if(contains(v)) {
-//            removeEdges(v);
-//            int item = _vertices.indexOf(v);
-//            _vertices.remove(item);
-//            _adjList.set(v, new ArrayList<>());
-//            clearUpEdges(v);
-//        }
-//        // FIXME FIXED
-//        clearUp();
         if (contains(v)) {
-            // remove from _adjacent list
             removeAdjacentList(v);
             removeEdges(v);
-            // remove from _vertices list
-//            int vIndex = _vertices.indexOf(v);
-//            _vertices.set(vIndex, 0);
             _vertices.remove(Integer.valueOf(v));
         }
-        // FIXME FIXED
     }
 
     /** NEW Implemented method. */
     private void removeAdjacentList(int v) {
-        // remove the v from adjacent list by index
         int vIndex = _vertices.indexOf(v);
-//        _adjList.remove(vIndex);
         _adjList.set(vIndex, new ArrayList<>());
-        // remove v from other's adjacent list
         for (ArrayList<Integer> list : _adjList) {
             list.remove(Integer.valueOf(v));
         }
     }
 
     /** Implemented helper method for remove(int v). */
-    private void clearUp() {
-        if (vertexSize() == 0) {
-            _adjList.clear();
-            _vertices.clear();
-            _edges.clear();
-        }
-    }
-
-    /** Implemented helper method for remove(int v). */
-    private void clearUpEdges(int v) {
-        for(int i = 0; i < _adjList.size(); i++){
-            ArrayList<Integer> adj = _adjList.get(i);
-            if (adj != null && adj.contains(v)) {
-                int removeThis = adj.indexOf(v);
-                adj.remove(removeThis);
-            }
-        }
-    }
-
-    /** Implemented helper method for remove(int v). */
     private void removeEdges(int v) {
-//        ArrayList<int[]> updated = new ArrayList<>();
-//        for(int i = 0; i < _edges.size(); i++){
-//            int[] pairOfEdges = _edges.get(i);
-//
-//            if (pairOfEdges[0] != v && pairOfEdges[1] != v) {
-//                updated.add(pairOfEdges);
-//            }
-//        }
-//        _edges = updated;
         if (!_edges.isEmpty()) {
             ArrayList<int[]> updated = new ArrayList<>();
             for (int i = 0; i < _edges.size(); i++) {
@@ -335,15 +185,6 @@ abstract class GraphObj extends Graph {
 
     @Override
     public void remove(int u, int v) {
-//        if (contains(u, v)) {
-//            _adjList.get(u).remove(v);
-//            if(edgeContains(u, v)) {
-//                int item = edgeIndex(u ,v);
-//                _edges.remove(item);
-//            }
-//        }
-//        // FIXME FIXED
-//        clearUp();
         if (contains(u, v)) {
             if (edgeContains(u, v)) {
                 int item = edgeIndex(u, v);
@@ -370,14 +211,11 @@ abstract class GraphObj extends Graph {
 
     @Override
     public Iteration<Integer> vertices() {
-        // FIXME FIXED
         return Iteration.iteration(_vertices);
     }
 
     @Override
     public Iteration<Integer> successors(int v) {
-        // FIXME FIXED
-//        return Iteration.iteration(_adjList.get(v));
         ArrayList<Integer> filled = new ArrayList<>();
         int maxValue = max(_vertices);
         for (int i=1; i<=maxValue; i++){
@@ -399,20 +237,16 @@ abstract class GraphObj extends Graph {
 
     @Override
     public Iteration<int[]> edges() {
-        // FIXME FIXED
         return Iteration.iteration(_edges);
     }
 
     @Override
     protected void checkMyVertex(int v) {
-        // FIXME FIXED
         super.checkMyVertex(v);
     }
 
     @Override
     protected int edgeId(int u, int v) {
-        // FIXME FIXED
-        // Cantor pairing
         if (!isDirected()){
             int max = Math.max(u,v);
             int min = Math.min(u,v);
@@ -421,5 +255,4 @@ abstract class GraphObj extends Graph {
         }
         return (u + v) * (u + v + 1) * u / 2;
     }
-    // FIXME FIXED
 }
--- proj3/graph/GraphTest.java
+++ proj3/graph/GraphTest.java
@@ -141,7 +141,7 @@ public class GraphTest {
             assertEquals("Current graph has vertex using iteration",i, v);
             i++;
         }
-        emptyGraph.add();// add 1
+        emptyGraph.add();
         assertEquals("Current graph has vertex using iteration",Integer.valueOf(1), emptyGraph.vertices().next());
     }
 
@@ -150,59 +150,12 @@ public class GraphTest {
     @Test
     public void undirectedTest() {
         UndirectedGraph g = new UndirectedGraph();
-//        g.add();
-//        assertEquals(1, g.vertexSize());
-//        assertEquals(0, g.edgeSize());
-//        g.remove(1);
-//        assertEquals(0, g.vertexSize());
-//        assertEquals(0, g.edgeSize());
         g.add();
         g.add();
         g.add(1,2);
         g.add(2,1);
         assertEquals(2, g.vertexSize());
         assertEquals(1, g.edgeSize());
-//        g.add();
-//        assertEquals(3, g.vertexSize());
-//        assertEquals(3, g.maxVertex());
-//
-//        g.add(3,1);
-//        g.add(3,4);
-//        assertEquals(4, g.vertexSize());
-//        assertEquals(4, g.maxVertex());
-//        assertEquals(3, g.edgeSize());
-//        g.add(6, 6);
-//        assertEquals(5, g.vertexSize());
-//        assertEquals(5, g.add());
-//        g.add(6,2);
-//        assertEquals(5,g.edgeSize());
-//        g.remove(7);
-//        assertEquals(6, g.vertexSize());
-//        g.remove(6);
-//        assertEquals(5, g.vertexSize());
-//        assertEquals(3, g.edgeSize());
-//        g.add(5,6);
-//        assertEquals(6, g.vertexSize());
-//        assertEquals(4, g.edgeSize());
-
-//        Iteration<Integer> iteration = g.successors(5);
-//        int count = 0;
-//        while (iteration.hasNext()) {
-//            iteration.next();
-//            count ++;
-//        }
-//        assertEquals(1, count);
-//
-//        Iteration<Integer> iteration1 = g.successors(6);
-//        count = 0;
-//        while (iteration1.hasNext()) {
-//            iteration1.next();
-//            count ++;
-//        }
-//        assertEquals(1, count);
-//        g.remove(1);
-//        assertEquals(5, g.vertexSize());
-//        assertEquals(2, g.edgeSize());
     }
 
     @Test
@@ -216,10 +169,6 @@ public class GraphTest {
         g.remove(1);
         assertEquals("Current graph has vertices", 0, g.vertexSize());
         assertEquals("Current graph has edges",0, g.edgeSize());
-//        g.add(2,3);
-//        assertEquals("Current graph has vertices", 2, g.vertexSize());
-//        assertEquals("Current graph has edges",1, g.edgeSize());
-//        assertTrue(g.isDirected());
     }
 
     @Test
