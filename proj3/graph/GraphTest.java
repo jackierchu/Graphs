56c195135da588a77347b373307c4a5afa1732a5
Jacqueline Chu <jackiechu@berkeley.edu>
2018-12-04 16:25:57 -0800

    message

--- proj3/graph/GraphTest.java
+++ proj3/graph/GraphTest.java
@@ -52,6 +52,22 @@ public class GraphTest {
 
     }
 
+
+    @Test(timeout = 1000)
+    public void undirectedGraphTestTogether() {
+        UndirectedGraph g = initializeUndirectedGraph();
+        g.add(5, 5);
+        g.add(1, 2);
+        g.add(2, 3);
+        g.add(1, 3);
+        g.add(1, 4);
+        g.remove(1);
+        g.add();
+        assertEquals("Edge size error", 2, g.edgeSize());
+        assertEquals("Max Vertex error", 5, g.maxVertex());
+        assertFalse("Edge contains error", g.contains(1, 3));
+    }
+
     @Test
     public void undirectedGraphAdd() {
         UndirectedGraph g = new UndirectedGraph();
@@ -182,5 +198,132 @@ public class GraphTest {
         assertEquals("Current graph has edges", expected, result);
     }
 
+    @Test(timeout = 1000)
+    public void directedGraphAddVertex() {
+        DirectedGraph g = new DirectedGraph();
+        int count = 5;
+        while (count > 0) {
+            g.add();
+            count--;
+        }
+        assertEquals("Directed Graph Add Vertex wrong size",
+                5, g.vertexSize());
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphRemoveVertex() {
+        DirectedGraph g = new DirectedGraph();
+        g.add();
+        g.add();
+        g.add();
+        assertEquals("Directed Graph remove Vertex wrong size",
+                3, g.vertexSize());
+        assertTrue("Directed Graph remove item error", g.contains(2));
+        g.remove(2);
+        assertEquals("Directed Graph remove Vertex wrong size",
+                2, g.vertexSize());
+        assertTrue("Directed Graph remove item error", g.contains(3));
+        assertFalse("Directed Graph remove item error", g.contains(2));
+        assertEquals("Directed Graph remove Vertex wrong size",
+                2, g.vertexSize());
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphRemoveAndAddKeepMin() {
+        DirectedGraph g = new DirectedGraph();
+        int count = 5;
+        while (count > 0) {
+            g.add();
+            count--;
+        }
+        g.remove(1);
+        g.remove(2);
+        g.remove(3);
+        g.remove(4);
+        g.add();
+        assertTrue("DirectedGraph Add number after remove, should keep min",
+                g.contains(1));
+        assertTrue("DirectedGraph Add number after remove, should keep min",
+                g.contains(5));
+        assertFalse("DirectedGraph Add number after remove, should keep min",
+                g.contains(6));
+        assertFalse("DirectedGraph Add number after remove, should keep min",
+                g.contains(4));
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphAddEdge() {
+        DirectedGraph g = new DirectedGraph();
+        int count = 5;
+        while (count > 0) {
+            g.add();
+            count--;
+        }
+        g.add(1, 2);
+        g.add(2, 3);
+        assertTrue("DirectedGraphAddEdge Error", g.contains(2, 3));
+        assertFalse("DirectedGraphAddEdge Error", g.contains(3, 2));
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphRemoveEdge() {
+        DirectedGraph g = new DirectedGraph();
+        g.add();
+        g.add();
+        g.add();
+        g.add(1, 2);
+        assertEquals("DG remove edge error", 1, g.edgeSize());
+        g.remove(1, 2);
+        assertEquals("DG remove edge error", 0, g.edgeSize());
+    }
+
+    @Test(timeout = 1000)
+    public void undirectedGraphEdgeId() {
+        UndirectedGraph g = initializeUndirectedGraph();
+        int id1 = g.edgeId(1, 2);
+        int id2 = g.edgeId(1, 2);
+        int id3 = g.edgeId(2, 1);
+        assertEquals("Direct Graph EdgeId Error", id1, id2);
+        assertEquals("Direct Graph EdgeId Error", id2, id3);
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphEdgeId() {
+        DirectedGraph g = new DirectedGraph();
+        int count = 5;
+        while (count > 0) {
+            g.add();
+            count--;
+        }
+        int id1 = g.edgeId(1, 2);
+        int id2 = g.edgeId(1, 2);
+        int id3 = g.edgeId(2, 1);
+        assertEquals("Direct Graph EdgeId Error", id1, id2);
+        assertNotEquals("Direct Graph EdgeId Error", id1, id3);
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphTestSuccessors() {
+        DirectedGraph g = new DirectedGraph();
+        g.add();
+        g.add();
+        g.add();
+        g.add(1, 2);
+        g.add(1, 3);
+        assertEquals("DirectedGraph Successors error", 2, g.outDegree(1));
+    }
+
+    @Test(timeout = 1000)
+    public void directedGraphTestPredecessors() {
+        DirectedGraph g = new DirectedGraph();
+        g.add();
+        g.add();
+        g.add();
+        g.add(1, 2);
+        g.add(1, 3);
+        g.add(2, 1);
+        g.add(3, 1);
+        assertEquals("DirectedGraph Successors error", 2, g.inDegree(1));
+    }
 
 }
